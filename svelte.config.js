77fa1538b1406c11641ef680d926c62efff1f911
silvestrevivo <silvestrevivomillan@gmail.com>
2022-11-01 15:46:40 +0100

    tw,eslint,prettier and alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+      $types: 'src/types/',
+      $blocks: 'src/blocks/',
+      $actions: 'src/actions/',
+      $utils: 'src/utils/',
+      $stores: 'src/stores/',
+      $icons: 'src/icons/',
+    },
 	}
 };
 
