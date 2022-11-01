3f4d948c6adde6150ce74c4e494860c3d5d2fd54
silvestrevivo <silvestrevivomillan@gmail.com>
2022-11-01 16:47:11 +0100

    feat(commitizen): conventional commits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div
+  class="text-center max-w-6xl mx-auto rounded-lg mt-8 p-4 space-y-4 bg-gray-100 shadow-lg"
+>
+  <h1 class="uppercase text-red-500">SvelteKit starter</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev" class="text-red-500 hover:underline"
+      >kit.svelte.dev</a
+    > to read the documentation
+  </p>
+</div>
